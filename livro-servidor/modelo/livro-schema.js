89cb8105eca68ffa5d3ef7df4640fb386943ea8d
DHiramIII <viniciusbrsampa@gmail.com>
2024-11-18 23:34:50 -0300

    first commit

---
+++ livro-servidor/modelo/livro-schema.js
@@ -0,0 +1,24 @@
+const banco = require("./conexao");
+const LivroSchema = new banco.Schema({
+  
+    titulo: {
+        type: String,
+        required: true
+    },
+    codEditora: {
+        type: Number,
+        required: true
+    },
+    resumo: {
+        type: String,
+        required: true
+    },
+    autores: {
+        type: [String],
+        required: true
+    }
+});
+
+const Livro = banco.model("livros", LivroSchema);
+
+module.exports = Livro;
